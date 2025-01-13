030c96ecea81bb0d4cd197f3b709388fa10baeae
juan cruz eusebi <jcrudo98@hotmail.com>
2025-01-13 17:17:23 -0300

    Agregar archivos de proyecto.

---
+++ practicaApiC#/ComicProcessor.cs
@@ -0,0 +1,43 @@
+﻿using DemoLibrary;
+using System;
+using System.Collections.Generic;
+using System.DirectoryServices.ActiveDirectory;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practicaApiC_
+{
+    public class ComicProcessor
+    {
+        
+        public static async Task<ComicModel> LoadComic(int comicNumber = 0)
+        {
+            string url = "";
+
+            if(comicNumber > 0)
+            {
+                url = $"https://xkcd.com/{ comicNumber }/info.0.json";
+            }
+            else
+            {
+                url = $"https://xkcd.com/info.0.json";
+            }
+            using (HttpResponseMessage reponse = await ApiHelper.ApiClient.GetAsync(url))
+            {
+                if (reponse.IsSuccessStatusCode)
+                {
+                    ComicModel comic = await reponse.Content.ReadFromJsonAsync<ComicModel>();
+
+                    return comic;
+                }
+                else
+                {
+                    throw new Exception(reponse.ReasonPhrase);
+                }
+            }
+        }
+    }
+}
---
+++ practicaApiC#/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using DemoLibrary;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace practicaApiC_
+{
+    public partial class MainWindow : Window
+    {
+        private int maxNumber = 0;
+        private int currentNumber = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            ApiHelper.InitializeClient();
+        }
+
+        private async Task LoadImage(int imageNumber = 0)
+        {
+            var comic = await ComicProcessor.LoadComic(imageNumber);
+
+            if (imageNumber == 0)
+            {
+                maxNumber = comic.Num;
+            }
+
+            currentNumber = comic.Num;
+
+            var uriSource = new Uri(comic.Img, UriKind.Absolute);
+            comicImage.Source = new BitmapImage(uriSource);
+
+        }
+
+        private async void window_Loaded(object sender, RoutedEventArgs e)
+        {
+            await LoadImage();
+        }
+
+    }
+}
